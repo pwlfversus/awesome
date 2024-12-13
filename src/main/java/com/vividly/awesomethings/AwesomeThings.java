cfe58b624d5a2047501482f61b46e5765f0e166e
Luca Rossini <hello.lucarossini@gmail.com>
2024-12-13 22:10:10 +0100

    (+) Added the Pink Garnet Creative Menu

--- src/main/java/com/vividly/awesomethings/AwesomeThings.java
+++ src/main/java/com/vividly/awesomethings/AwesomeThings.java
@@ -1,6 +1,7 @@
 package com.vividly.awesomethings;
 
 import com.vividly.awesomethings.block.ModBlocks;
+import com.vividly.awesomethings.item.ModItemGroups;
 import com.vividly.awesomethings.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,8 @@ public class AwesomeThings implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
