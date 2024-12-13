fbc10d732b7a92c930839c94d85eae4adc4ee1ca
Luca Rossini <hello.lucarossini@gmail.com>
2024-12-13 20:24:25 +0100

    Learning FabricMC

---
+++ src/main/java/com/vividly/awesomethings/AwesomeThings.java
@@ -0,0 +1,16 @@
+package com.vividly.awesomethings;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AwesomeThings implements ModInitializer {
+	public static final String MOD_ID = "awesomethings";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/vividly/awesomethings/AwesomeThingsClient.java
@@ -0,0 +1,10 @@
+package com.vividly.awesomethings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AwesomeThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
